dc61769c78486dc7eae0123c758183edd350c34a
P Dineshkumar <cheenu2801@gmail.com>
2024-09-02 20:42:51 +0530

    Improving on it

--- store_index.py
+++ store_index.py
@@ -9,3 +9,9 @@ extracted_data = load_pdf("../data/")
 text_chunks = text_split(extracted_data)
 embeddings = embedding_model()
 
+def vector_embedding():
+  store = FAISS.from_documents(text_chunks,embeddings)
+  embedding = store.as_retriever()
+  return embedding
+vector_database = vector_embedding()
+
